5477a6164466043ef9c0c3d24ee7285b5474dcb2
Mindaugas <min2sia@gmail.com>
2017-04-24 21:16:38 +0300

    Chapters, navigation arrows

--- main.go
+++ main.go
@@ -23,9 +23,11 @@ type Book struct {
 
 // Chapter - individual chapter in a book
 type Chapter struct {
-	Num    int
-	Name   string
-	Verses []Verse
+	Num         int
+	Name        string
+	Verses      []Verse
+	PrevChapter int
+	NextChapter int
 }
 
 // Verse - individual verse in a chapter
@@ -39,10 +41,16 @@ type Verse struct {
 	SynonymsTranslation   []string
 	Translation           template.HTML
 	Purport               []template.HTML
+	PrevVerse             [2]int // [ChapterNum, VerseNum] - Num, not Idx!
+	NextVerse             [2]int
 }
 
 // BG is a top-level structure of the book
 var BG Book
+
+//TODO: title & keywords by language
+var pageTitle = "Bhagavad-Gita As It Is. Translation and commentaries by HDG A.C.Bhaktivedanta Swami Prabhupada"
+var pageKeywords = "Bhagavad Gita As It Is, Bhagavad-gītā, Bhagavad Gita, gita, ISKCON, Prabhupada, A.C. Bhaktivedanta Swami"
 
 // defaultLangID - default to LT if no language specified
 var defaultLangID = "lt"
@@ -90,6 +98,47 @@ func loadJSON() {
 	if err := json.Unmarshal(data, &BG); err != nil {
 		log.Fatalf("JSON unmarshalling failed: %s", err)
 	}
+
+	// Set Prev and Next link values:
+	for chapterIdx := range BG.Chapters {
+		BG.Chapters[chapterIdx].PrevChapter = BG.Chapters[chapterIdx].Num - 1
+		BG.Chapters[chapterIdx].NextChapter = BG.Chapters[chapterIdx].Num + 1
+		if chapterIdx == 0 {
+			BG.Chapters[chapterIdx].PrevChapter = 0
+		} else if chapterIdx == len(BG.Chapters)-1 {
+			BG.Chapters[chapterIdx].NextChapter = 0
+		}
+		for verseIdx, verse := range BG.Chapters[chapterIdx].Verses {
+
+			// Default values for most of verses
+			verse.PrevVerse[0] = chapterIdx + 1
+			verse.PrevVerse[1] = verseIdx + 1 - 1
+			verse.NextVerse[0] = chapterIdx + 1
+			verse.NextVerse[1] = verseIdx + 1 + 1
+
+			// Exceptions
+			if chapterIdx == 0 && verseIdx == 0 {
+				// Very first verse - no Prev
+				verse.PrevVerse[0] = 0
+				verse.PrevVerse[1] = 0
+			} else if chapterIdx == len(BG.Chapters)-1 && verseIdx == len(BG.Chapters[chapterIdx].Verses)-1 {
+				// Very last verse - no Next
+				verse.NextVerse[0] = 0
+				verse.NextVerse[1] = 0
+			} else if verseIdx == 0 {
+				// First verse of a chapter
+				verse.PrevVerse[0]--
+				verse.PrevVerse[1] = len(BG.Chapters[chapterIdx-1].Verses)
+			} else if verseIdx == len(BG.Chapters[chapterIdx].Verses)-1 {
+				// Last verse of a chapter
+				verse.NextVerse[0]++
+				verse.NextVerse[1] = 1
+			}
+
+			BG.Chapters[chapterIdx].Verses[verseIdx].PrevVerse = verse.PrevVerse
+			BG.Chapters[chapterIdx].Verses[verseIdx].NextVerse = verse.NextVerse
+		}
+	}
 }
 
 // IndexHandler - default route "/" handler
@@ -104,6 +153,7 @@ func ChapterHandler(w http.ResponseWriter, r *http.Request) {
 
 	if chapterNum == 0 || chapterNum > len(BG.Chapters) {
 		fmt.Fprintf(w, "Chapter %v does not exist!\n", vars["chapter"])
+		//TODO: redirect
 	} else {
 		url, err := router.Get("langChapter").URL("language", defaultLangID, "chapter", vars["chapter"])
 		if err != nil {
@@ -119,12 +169,75 @@ func LangChapterHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	//fmt.Fprintf(w, "LangChapterHandler: %v<br>", vars)
 	chapterNum, _ := strconv.Atoi(vars["chapter"])
-	//langId := vars["language"]
 
-	if chapterNum == 0 || chapterNum > len(BG.Chapters) {
+	if chapterNum < 1 || chapterNum > len(BG.Chapters) {
 		fmt.Fprintf(w, "Chapter %v does not exist!\n", vars["chapter"])
+		//TODO: redirect
 	} else {
-		fmt.Fprintf(w, "[%s] %v. %s\n", vars["language"], chapterNum, BG.Chapters[chapterNum-1].Name)
+		chapter := BG.Chapters[chapterNum-1]
+		// fmt.Fprintf(w, "[%s] %v. %s\n", vars["language"], chapterNum, BG.Chapters[chapterNum-1].Name)
+
+		fp := path.Join("templates", "chapter.html")
+		tmpl, err := template.ParseFiles(fp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Create navigation arrows
+		var prevHref, nextHref, upHref template.HTML
+		if chapter.PrevChapter > 0 {
+			prevURL, err := router.Get("langChapter").URL("language", vars["language"], "chapter", strconv.Itoa(chapter.PrevChapter))
+			if err != nil {
+				panic(err)
+			}
+			prevHref = template.HTML(fmt.Sprintf(`<a href="%s">&lt;&lt;</a>`, prevURL.String()))
+		} else {
+			prevHref = `&lt;&lt;`
+		}
+
+		if chapter.NextChapter > 0 {
+			nextURL, err := router.Get("langChapter").URL("language", vars["language"], "chapter", strconv.Itoa(chapter.NextChapter))
+			if err != nil {
+				panic(err)
+			}
+			nextHref = template.HTML(fmt.Sprintf(`<a href="%s">&gt;&gt;</a>`, nextURL.String()))
+		} else {
+			nextHref = `&gt;&gt;`
+		}
+
+		//TODO: TOC route?
+		// upURL, err := router.Get("langChapterVerse").URL("language", vars["language"], "chapter", strconv.Itoa(verse.NextVerse[0]), "verse", strconv.Itoa(verse.NextVerse[1]))
+		// if err != nil {
+		// 	panic(err)
+		// }
+		upHref = template.HTML(fmt.Sprintf(`<a href="%s">^</a>`, "/"))
+
+		// construct verses list
+		var versesList []template.HTML
+		for _, verse := range chapter.Verses {
+			verseURL, err := router.Get("langChapterVerse").URL("language", vars["language"], "chapter", strconv.Itoa(chapter.Num), "verse", strconv.Itoa(verse.Num))
+			if err != nil {
+				panic(err)
+			}
+			verseHref := template.HTML(fmt.Sprintf(`<a href="%s">%v.%v</a>`, verseURL.String(), chapter.Num, verse.Num))
+			versesList = append(versesList, verseHref+" "+verse.Translation)
+		}
+		data := map[string]interface{}{
+			"languageId":  vars["language"],
+			"title":       pageTitle,
+			"keywords":    pageKeywords,
+			"chapterNum":  chapter.Num,
+			"chapterName": chapter.Name,
+			"verses":      versesList,
+			"next":        nextHref,
+			"prev":        prevHref,
+			"up":          upHref,
+		}
+
+		if err := tmpl.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	}
 }
 
@@ -136,8 +249,10 @@ func ChapterVerseHandler(w http.ResponseWriter, r *http.Request) {
 
 	if chapterNum == 0 || chapterNum > len(BG.Chapters) {
 		fmt.Fprintf(w, "Chapter %v does not exist!\n", vars["chapter"])
+		//TODO: redirect
 	} else if verseNum == 0 || verseNum > len(BG.Chapters[chapterNum-1].Verses) {
 		fmt.Fprintf(w, "Verse %v.%v does not exist!\n", vars["chapter"], vars["verse"])
+		//TODO: redirect
 	} else {
 		url, err := router.Get("langChapterVerse").URL("language", defaultLangID, "chapter", vars["chapter"], "verse", vars["verse"])
 		if err != nil {
@@ -156,10 +271,11 @@ func LangChapterVerseHandler(w http.ResponseWriter, r *http.Request) {
 
 	if chapterNum == 0 || chapterNum > len(BG.Chapters) {
 		fmt.Fprintf(w, "Chapter %v does not exist!\n", vars["chapter"])
+		//TODO: redirect
 	} else if verseNum == 0 || verseNum > len(BG.Chapters[chapterNum-1].Verses) {
 		fmt.Fprintf(w, "Verse %v.%v does not exist!\n", vars["chapter"], vars["verse"])
+		//TODO: redirect
 	} else {
-
 		fp := path.Join("templates", "verse.html")
 		tmpl, err := template.ParseFiles(fp)
 		if err != nil {
@@ -167,52 +283,38 @@ func LangChapterVerseHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		prevChapterNum := chapterNum
-		prevVerseNum := verseNum - 1
-		if chapterNum == 1 && verseNum == 1 { // already first chapter - no previous
-			prevChapterNum = 0
-			prevVerseNum = 0
-		} else {
-			if prevVerseNum < 1 {
-				prevChapterNum--
-				prevVerseNum = len(BG.Chapters[prevChapterNum-1].Verses)
+		verse := BG.Chapters[chapterNum-1].Verses[verseNum-1]
+
+		// fmt.Printf("Prev: %#v.%#v  Next: %#v.%#v \n", verse.PrevVerse[0], verse.PrevVerse[1], verse.NextVerse[0], verse.NextVerse[1])
+
+		var prevHref, nextHref, upHref template.HTML
+		if verse.PrevVerse[1] > 0 {
+			prevURL, urlErr := router.Get("langChapterVerse").URL("language", vars["language"], "chapter", strconv.Itoa(verse.PrevVerse[0]), "verse", strconv.Itoa(verse.PrevVerse[1]))
+			if urlErr != nil {
+				panic(urlErr)
 			}
-		}
-
-		nextChapterNum := chapterNum
-		nextVerseNum := verseNum + 1
-		if nextVerseNum > len(BG.Chapters[chapterNum-1].Verses) {
-			nextVerseNum = 1
-			nextChapterNum++
-		}
-		if nextChapterNum > len(BG.Chapters) { // already last chapter - no next
-			nextVerseNum = 0
-			nextChapterNum = 0
-		}
-
-		prevURL, err := router.Get("langChapterVerse").URL("language", vars["language"], "chapter", strconv.Itoa(prevChapterNum), "verse", strconv.Itoa(prevVerseNum))
-		if err != nil {
-			panic(err)
-		}
-		nextURL, err := router.Get("langChapterVerse").URL("language", vars["language"], "chapter", strconv.Itoa(nextChapterNum), "verse", strconv.Itoa(nextVerseNum))
-		if err != nil {
-			panic(err)
-		}
-
-		var prevHref, nextHref template.HTML
-		if prevChapterNum > 0 {
 			prevHref = template.HTML(fmt.Sprintf(`<a href="%s">&lt;&lt;</a>`, prevURL.String()))
 		} else {
 			prevHref = `&lt;&lt;`
 		}
 
-		if nextChapterNum > 0 {
+		if verse.NextVerse[1] > 0 {
+			nextURL, urlErr := router.Get("langChapterVerse").URL("language", vars["language"], "chapter", strconv.Itoa(verse.NextVerse[0]), "verse", strconv.Itoa(verse.NextVerse[1]))
+			if urlErr != nil {
+				panic(urlErr)
+			}
 			nextHref = template.HTML(fmt.Sprintf(`<a href="%s">&gt;&gt;</a>`, nextURL.String()))
 		} else {
 			nextHref = `&gt;&gt;`
 		}
 
-		// Make Synonyms section
+		upURL, urlErr := router.Get("langChapter").URL("language", vars["language"], "chapter", strconv.Itoa(verse.NextVerse[0]))
+		if urlErr != nil {
+			panic(urlErr)
+		}
+		upHref = template.HTML(fmt.Sprintf(`<a href="%s">^</a>`, upURL.String()))
+
+		// Join Synonyms string
 		synonyms := ""
 		separator := "; "
 		for i, v := range BG.Chapters[chapterNum-1].Verses[verseNum-1].SynonymsSanskrit {
@@ -222,17 +324,17 @@ func LangChapterVerseHandler(w http.ResponseWriter, r *http.Request) {
 			synonyms += v + "—" + BG.Chapters[chapterNum-1].Verses[verseNum-1].SynonymsTranslation[i] + separator
 		}
 
-		//TODO: title & keywords by language
 		data := map[string]interface{}{
 			"languageId": vars["language"],
-			"title":      "Bhagavad-Gita As It Is. Translation and commentaries by HDG A.C.Bhaktivedanta Swami Prabhupada",
-			"keywords":   "Bhagavad Gita As It Is, Bhagavad-gītā, Bhagavad Gita, gita, ISKCON, Prabhupada, A.C. Bhaktivedanta Swami",
+			"title":      pageTitle,
+			"keywords":   pageKeywords,
 			"chapterNum": chapterNum,
 			"verseNum":   verseNum,
 			"synonyms":   synonyms,
 			"verse":      BG.Chapters[chapterNum-1].Verses[verseNum-1],
 			"next":       nextHref,
 			"prev":       prevHref,
+			"up":         upHref,
 		}
 
 		if err := tmpl.Execute(w, data); err != nil {
